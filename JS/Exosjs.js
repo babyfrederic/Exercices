b36565175304bbae1306623429804b6d115d0980
babyfrederic <62266289+babyfrederic@users.noreply.github.com>
2020-12-19 16:28:02 +0100

    Add files via upload

---
+++ JS/Exosjs.js
@@ -0,0 +1,18 @@
+// JS des Exercices)
+// JS Exo5
+let inputa = document.getElementById("myInputx");
+fact =1 ;
+inputa.addEventListener("click", ()=>{
+    let nbr=  Number(document.getElementById("formx").value);
+ 
+while nbr>1 {
+    
+    facoriel =+ factoriel * nbr ;
+    nb =- nb - 1;
+    document.getElementById("demox");
+    }
+function effacer() 
+    {
+        window.location.href="../file/Exo5.html";
+    }
+    document.getElementById("efface").addEventListener("click", effacer);  
